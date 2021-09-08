7dc148d9b7028bdf453402e8b9ea8fef5884c667
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-09-08 13:30:29 -0400

    Refactor: fogefoge

--- ruby-base/alura/fogefoge/fogefoge.rb
+++ ruby-base/alura/fogefoge/fogefoge.rb
@@ -8,14 +8,26 @@ end
 
 def encontra_jogador(mapa)
   caractere_heroi = "H"
-  for linha = 0..(mapa.size-1)
-    linha_atual = mapa[linha]
+  mapa.each_with_index do |linha_atual, linha|
     coluna_do_heroi = linha_atual.index caractere_heroi
       if coluna_do_heroi
-        #ache linha e co
+        return [linha, coluna_do_heroi]
       end
   end
   #nao ache
+end
+
+def calcula_nova_posicao(heroi. direcao)
+  case direcao
+    when 'W'
+      heroi[0] -= 1
+    when 'S'
+      heroi[0] += 1
+    when 'A'
+      heroi[0] -= 1
+    when 'D'
+      heroi[0] += 1
+  end
 end
 
 def joga(nome)
@@ -24,6 +36,27 @@ def joga(nome)
   while true
     desenha mapa
     direcao = pede_movimento
+    heroi = encontra_jogador mapa
+    mapa[heroi[0]] [heroi[1]] = ''
+    nova_posicao = calcula_nova_posicao heroi, direcao
+    if nova_posicao[0] < 0
+      next
+    end
+    if nova_posicao[1] < 0
+      next
+    end
+    if nova_posicao[0] < mapa.size
+      next
+    end
+    if nova_posicao[0] < mapa[0].size
+      next
+    end
+    if mapa[nova_posicao[0]] < mapa[0] [nova_posicao[1]] == 'X'
+      next
+    end
+
+    mapa[heroi[0]] [heroi[1]] = ''
+    mapa[nova_posicao[0]] [nova_posicao[1]] = 'H'
   end
 end
 
