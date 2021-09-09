3173546ffabc65d5bdffe87b9fda043eb76fd1c9
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-09-09 13:52:21 -0400

    Refactor: advinha

--- ruby-base/alura/adivinha/maior_ou_menor.rb
+++ ruby-base/alura/adivinha/maior_ou_menor.rb
@@ -3,18 +3,11 @@ puts "Qual é o seu nome?"
 nome = gets
 
 puts "Começaremos o jogo para você, #{nome}"
-puts
-puts
-puts
-puts
-puts
+puts "\n\n\n\n\n\n"
 puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
-puts
-puts
-puts
-puts
+puts "\n\n\n\n\n\n"
 puts "Tentativa 1"
 puts "Entre com o numero: "
 chute = gets
