a4bd5e0c74d2db4a67122288f54085fd078e71a2
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-09-09 14:52:20 -0400

    Feat: game in ruby

--- ruby-base/alura/adivinha/maior_ou_menor.rb
+++ ruby-base/alura/adivinha/maior_ou_menor.rb
@@ -8,21 +8,24 @@ puts "Escolhendo um número secreto entre 0 e 200..."
 numero_secreto = 175
 puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
 puts "\n\n\n\n\n\n"
-puts "Tentativa 1"
-puts "Entre com o numero: "
-chute = gets
+for tentativa in 1..5
+  puts "Tentativa #{tentativa.to_s} de 5"
+  puts "Entre com o numero: "
+  chute = gets
 
-puts "Será que acertou? Você chutou #{chute}"
+  puts "Será que acertou? Você chutou #{chute}"
 
-acertou = chute.to_i == numero_secreto
+  acertou = chute.to_i == numero_secreto
 
-if acertou
-  puts "Parabéns #{nome} você acertou!"
-else
-  maior = numero_secreto > chute.to_i
-  if maior
-    puts 'O número secreto é maior!'
+  if acertou
+    puts "Parabéns #{nome} você acertou!"
   else
-    puts 'O número secreto é menor!'
+    maior = numero_secreto > chute.to_i
+    if maior
+      puts 'O número secreto é maior!'
+    else
+      puts 'O número secreto é menor!'
+    end
   end
 end
+
