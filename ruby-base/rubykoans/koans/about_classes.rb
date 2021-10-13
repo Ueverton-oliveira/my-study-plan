cf848ace28058958f5ee49a452dd426b4fa90067
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-10-13 06:00:30 -0400

    feature: classes test_different_objects_have_different_instance_variables

--- ruby-base/rubykoans/koans/about_classes.rb
+++ ruby-base/rubykoans/koans/about_classes.rb
@@ -87,7 +87,7 @@ class AboutClasses < Neo::Koan
     fido = Dog4.new
     fido.set_name("Fido")
 
-    assert_equal __, fido.name
+    assert_equal "Fido", fido.name
   end
 
   # ------------------------------------------------------------------
@@ -101,7 +101,7 @@ class AboutClasses < Neo::Koan
     fido = Dog5.new
 
     fido.name = "Fido"
-    assert_equal __, fido.name
+    assert_equal "Fido", fido.name
   end
 
   # ------------------------------------------------------------------
@@ -115,11 +115,11 @@ class AboutClasses < Neo::Koan
 
   def test_initialize_provides_initial_values_for_instance_variables
     fido = Dog6.new("Fido")
-    assert_equal __, fido.name
+    assert_equal "Fido", fido.name
   end
 
   def test_args_to_new_must_match_initialize
-    assert_raise(___) do
+    assert_raise(ArgumentError) do
       Dog6.new
     end
     # THINK ABOUT IT:
@@ -130,7 +130,7 @@ class AboutClasses < Neo::Koan
     fido = Dog6.new("Fido")
     rover = Dog6.new("Rover")
 
-    assert_equal __, rover.name != fido.name
+    assert_equal true, rover.name != fido.name
   end
 
   # ------------------------------------------------------------------
