b6a9a6304fa555f53e8cd0bb2d0206b401e6700b
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-10-20 06:08:44 -0400

    feature: test_constants_can_be_looked_up_explicitly

--- ruby-base/rubykoans/koans/about_scope.rb
+++ ruby-base/rubykoans/koans/about_scope.rb
@@ -68,8 +68,8 @@ class AboutScope < Neo::Koan
   end
 
   def test_constants_can_be_looked_up_explicitly
-    assert_equal __, PI == AboutScope.const_get("PI")
-    assert_equal __, MyString == AboutScope.const_get("MyString")
+    assert_equal true, PI == AboutScope.const_get("PI")
+    assert_equal true, MyString == AboutScope.const_get("MyString")
   end
 
   def test_you_can_get_a_list_of_constants_for_any_class_or_module
