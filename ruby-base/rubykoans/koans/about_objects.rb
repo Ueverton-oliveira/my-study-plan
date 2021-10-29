e68cf78e7e6ed86ecd766318826b6003a318027b
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-10-29 05:36:17 -0400

    feature: test_small_integers

--- ruby-base/rubykoans/koans/about_objects.rb
+++ ruby-base/rubykoans/koans/about_objects.rb
@@ -34,7 +34,7 @@ class AboutObjects < Neo::Koan
     assert_equal 1, 0.object_id
     assert_equal 3, 1.object_id
     assert_equal 5, 2.object_id
-    assert_equal __, 100.object_id
+    assert_equal 201, 100.object_id
 
     # THINK ABOUT IT:
     # What pattern do the object IDs for small integers follow?
