154dc3dbec6aa0a3fa1503543ee5dea20035e8a5
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-10-29 05:29:03 -0400

    feature: test_everything_is_an_object

--- ruby-base/rubykoans/koans/about_objects.rb
+++ ruby-base/rubykoans/koans/about_objects.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/neo')
 
 class AboutObjects < Neo::Koan
   def test_everything_is_an_object
-    assert_equal __, 1.is_a?(Object)
+    assert_equal true, 1.is_a?(Object)
     assert_equal __, 1.5.is_a?(Object)
     assert_equal __, "string".is_a?(Object)
     assert_equal __, nil.is_a?(Object)
