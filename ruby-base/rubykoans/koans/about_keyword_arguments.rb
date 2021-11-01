c848dbd7c60196adefb916296717e9327655232c
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-11-01 05:18:25 -0400

    feature: test_keyword_arguments_with_wrong_number_of_arguments

--- ruby-base/rubykoans/koans/about_keyword_arguments.rb
+++ ruby-base/rubykoans/koans/about_keyword_arguments.rb
@@ -18,7 +18,7 @@ class AboutKeywordArguments < Neo::Koan
   end
 
   def test_keyword_arguments_with_wrong_number_of_arguments
-    exception = assert_raise (___) do
+    exception = assert_raise (ArgumentError) do
       method_with_keyword_arguments_with_mandatory_argument
     end
     assert_match(/__/, exception.message)
