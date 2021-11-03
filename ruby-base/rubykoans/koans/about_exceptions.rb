f1287a94252cd564fccf7e67f797fd0c7e565b6f
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-11-03 05:20:09 -0400

    feature: test_asserting_an_error_is_raised

--- ruby-base/rubykoans/koans/about_exceptions.rb
+++ ruby-base/rubykoans/koans/about_exceptions.rb
@@ -60,7 +60,7 @@ class AboutExceptions < Neo::Koan
   # Sometimes, we must know about the unknown
   def test_asserting_an_error_is_raised
     # A do-end is a block, a topic to explore more later
-    assert_raise(___) do
+    assert_raise(MySpecialError) do
       raise MySpecialError.new("New instances can be raised directly.")
     end
   end
