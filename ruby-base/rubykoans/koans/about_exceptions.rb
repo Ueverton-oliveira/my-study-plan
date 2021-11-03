cf45c666d166e1175c3eda1d3d8d5d4367edbfeb
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-11-03 05:16:14 -0400

    feature(test_rescue_clause): Should be a Runtime Error

--- ruby-base/rubykoans/koans/about_exceptions.rb
+++ ruby-base/rubykoans/koans/about_exceptions.rb
@@ -23,7 +23,7 @@ class AboutExceptions < Neo::Koan
     assert_equal :exception_handled, result
 
     assert_equal true, ex.is_a?(StandardError), "Should be a Standard Error"
-    assert_equal __, ex.is_a?(RuntimeError),  "Should be a Runtime Error"
+    assert_equal true, ex.is_a?(RuntimeError),  "Should be a Runtime Error"
 
     assert RuntimeError.ancestors.include?(StandardError),
       "RuntimeError is a subclass of StandardError"
