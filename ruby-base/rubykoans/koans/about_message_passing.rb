e07961acdcd0ec65d281ffb4276e0bab7e355002
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-11-04 05:30:07 -0400

    Feature: test_foo_method_are_caught

--- ruby-base/rubykoans/koans/about_message_passing.rb
+++ ruby-base/rubykoans/koans/about_message_passing.rb
@@ -150,8 +150,8 @@ class AboutMessagePassing < Neo::Koan
   def test_foo_method_are_caught
     catcher = WellBehavedFooCatcher.new
 
-    assert_equal __, catcher.foo_bar
-    assert_equal __, catcher.foo_baz
+    assert_equal "Foo to you too", catcher.foo_bar
+    assert_equal "Foo to you too", catcher.foo_baz
   end
 
   def test_non_foo_messages_are_treated_normally
