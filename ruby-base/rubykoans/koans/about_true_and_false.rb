739fa01b6a6ab16af1662cc2c4746e7833c3bf96
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-11-09 08:23:05 -0400

    Featue: test_nil_is_hash

--- ruby-base/rubykoans/koans/about_true_and_false.rb
+++ ruby-base/rubykoans/koans/about_true_and_false.rb
@@ -25,7 +25,7 @@ class AboutTrueAndFalse < Neo::Koan
     assert_equal :true_stuff, truth_value(1)
     assert_equal :true_stuff, truth_value(0)
     assert_equal :true_stuff, truth_value([])
-    assert_equal __, truth_value({})
+    assert_equal :true_stuff, truth_value({})
     assert_equal __, truth_value("Strings")
     assert_equal __, truth_value("")
   end
