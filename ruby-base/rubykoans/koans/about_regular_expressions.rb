008585c2f7bd0643170aef19b36f8972daf02e88
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-12-08 05:26:54 -0400

    feat: test_sub_is_like_find_and_replace

--- ruby-base/rubykoans/koans/about_regular_expressions.rb
+++ ruby-base/rubykoans/koans/about_regular_expressions.rb
@@ -152,7 +152,7 @@ class AboutRegularExpressions < Neo::Koan
   end
 
   def test_sub_is_like_find_and_replace
-    assert_equal __, "one two-three".sub(/(t\w*)/) { $1[0, 1] }
+    assert_equal 'one t-three', "one two-three".sub(/(t\w*)/) { $1[0, 1] }
   end
 
   def test_gsub_is_like_find_and_replace_all
