970608740d04e8addd1ed4b272c847e2dade40d1
Ueverton-oliveira <ueverton.souz@gmail.com>
2021-12-14 05:13:41 -0400

    Feature: test_single_quoted_strings_do_not_interpola

--- ruby-base/rubykoans/koans/about_strings.rb
+++ ruby-base/rubykoans/koans/about_strings.rb
@@ -129,7 +129,7 @@ EOS
   def test_single_quoted_strings_do_not_interpolate
     value = 123
     string = 'The value is #{value}'
-    assert_equal __, string
+    assert_equal 'The value is #{value}', string
   end
 
   def test_any_ruby_expression_may_be_interpolated
