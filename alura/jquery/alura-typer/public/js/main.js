2dc4a46cd2c4eece8ee4a70b9baeb8e68d029692
Ueverton-oliveira <ueveron.souz@gmail.com>
2022-05-12 15:58:43 -0400

    feat: event click

--- alura/jquery/alura-typer/public/js/main.js
+++ alura/jquery/alura-typer/public/js/main.js
@@ -5,3 +5,14 @@ var numberWords = phrase.split(' ').length;
 var sizeWords = $('#tamanho-frase');
 
 sizeWords.text(numberWords);
+
+var campoDigitacao = $('.campo-digitacao');
+
+campoDigitacao.on("click", function(){
+  var conteudo = campoDigitacao.val();
+  var quantidadePalavras = conteudo.split(" ").length;
+    $("#contador-palavras").text(quantidadePalavras);
+
+  var quantidadeCaracteres = conteudo.length;
+    $("#contador-caracteres").text(quantidadeCaracteres);
+});
