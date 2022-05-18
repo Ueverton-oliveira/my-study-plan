216cb1bce9e1fb76a25c151474703e0288df7f43
Ueverton-oliveira <ueveron.souz@gmail.com>
2022-05-18 16:22:00 -0400

    feat: Listener event click

--- alura/javascript/introducao-javascript/js/index.js
+++ alura/javascript/introducao-javascript/js/index.js
@@ -41,3 +41,9 @@ for(var i = 0; i < pacientes.length; i++){
   }
 }
 
+//função anonima e função declarada
+titulo.addEventListener("click", mostraMensagem);
+
+function mostraMensagem(){
+  console.log("Olá eu fui clicado!");
+}
