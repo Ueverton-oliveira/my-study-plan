2c66582bd09b7edc91e52d78128c7c1641de995d
Ueverton-oliveira <ueveron.souz@gmail.com>
2022-09-08 15:04:05 -0400

    feat(ruby): array

---
+++ ruby-base/ruby/array.rb
@@ -0,0 +1,24 @@
+estados = []
+
+#Inserção de elementos
+estados.push('Espirito Santo')
+estados.push('Minas Gerais', 'Rio de Janeiro', 'São Paulo')
+estados << 'MS'
+estados.insert(0, 'Acre', 'Amapa')
+
+#mostrar elementos
+estados[1]
+estados[1..3]
+estados[-1] #ultimo elemento
+estados[-2..-5]
+estados.first
+estados.count
+estados.lenght
+estados.empty?
+estados.include?('São Paulo')
+
+#deletar elementos
+
+estados.delete_at(2)
+estado.pop #exclui o ultimo elemento
+estado.shift #exclui o primeiro elemento
