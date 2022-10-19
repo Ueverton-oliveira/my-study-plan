2ecea31921c13427fc32693c788e95554c876e50
Ueverton-oliveira <ueveron.souz@gmail.com>
2022-10-19 09:03:37 -0400

    config ci

--- ruby-base/ruby/array.rb
+++ ruby-base/ruby/array.rb
@@ -1,10 +1,13 @@
 estados = []
 
+days = %w{Monday Tuesday Wednesday Thursday Friday Saturday Sunday}
+
 #Inserção de elementos
 estados.push('Espirito Santo')
 estados.push('Minas Gerais', 'Rio de Janeiro', 'São Paulo')
 estados << 'MS'
 estados.insert(0, 'Acre', 'Amapa')
+estados.unshift('ms') #inseri na posição 0
 
 #mostrar elementos
 estados[1]
@@ -20,5 +23,16 @@ estados.include?('São Paulo')
 #deletar elementos
 
 estados.delete_at(2)
+estados.delete('ms')
 estado.pop #exclui o ultimo elemento
 estado.shift #exclui o primeiro elemento
+
+#conversão do elementos em string
+
+array = %w[U e v e r t o n]
+array.join #  "Ueverton"
+
+array.join('-') #"U-e-v-e-r-t-o-n"
+
+# metodos
+
