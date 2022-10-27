b6c9b8c832c774cc702363ea28c05c5e3ff503a4
Ueverton-oliveira <ueveron.souz@gmail.com>
2022-10-27 15:46:27 -0400

    feat(JS): comparation

---
+++ alura/javascript/course_alura/comparation.js
@@ -0,0 +1,4 @@
+// ||: Operador “ou”, retorna true caso uma condição seja válida;
+// &&: Operador “e”, retorna true somente se todas as condições forem válidas;
+// != e !==: Operadores “não igual” e “estritamente não igual”, utilizados para comparação,
+// da mesma forma que == e === retornam true ou false.
