fb9abc9a5bc47f70176de35f0f218280ea351819
Ueverton-oliveira <ueveron.souz@gmail.com>
2022-10-28 13:55:52 -0400

    feat: templates string

---
+++ alura/javascript/course_alura/template-string.js
@@ -0,0 +1,7 @@
+const name = 'Ueverton';
+const age = 31;
+const city = 'Dourados';
+
+let apresentation = `Meu nome é ${ name }, minha cidade é ${ city } e minha idade é ${ age } anos`;
+
+console.log(apresentation)
