7d86c0d72fe7074fee56948522658740de42336f
Ueverton-oliveira <ueverton.souz@gmail.com>
2024-02-20 15:59:16 -0400

    function

--- alura/go/hello/hello.go
+++ alura/go/hello/hello.go
@@ -1,11 +1,47 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	nome := "Ueverton"
-	idade := 32
-	versao := 1.22
-	fmt.Println("Olá Sr.", nome, "sua idade é", idade)
+
+	exibeIntroducao()
+	exibeMenu()
+	comando := leComando()
+
+	switch comando {
+	case 1:
+		fmt.Println("Monitorando...")
+	case 2:
+		fmt.Println("Exibindo Logs...")
+	case 0:
+		fmt.Println("Saindo do programa...")
+		os.Exit(0)
+	default:
+		fmt.Println("Não conheço este comando")
+		os.Exit(-1)
+	}
+}
+
+func exibeIntroducao() {
+	nome := "Douglas"
+	versao := 1.1
+	fmt.Println("Olá, sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
+}
+
+func exibeMenu() {
+	fmt.Println("1- Iniciar Monitoramento")
+	fmt.Println("2- Exibir Logs")
+	fmt.Println("0- Sair do Programa")
+}
+
+func leComando() int {
+	var comandoLido int
+	fmt.Scan(&comandoLido)
+	fmt.Println("O comando escolhido foi", comandoLido)
+
+	return comandoLido
 }
