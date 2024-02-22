4adb8bb9d62e63fcebb926105ade4546fe54a3d4
Ueverton-oliveira <ueverton.souz@gmail.com>
2024-02-22 09:15:05 -0400

    variables and types

---
+++ alura/go/banco/main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	var titular string = "Ueverton"
+	var numeroAgencia int = 589
+	var numeroConta int = 123456
+	var saldo float64 = 125.5
+
+	fmt.Println(titular, numeroAgencia, numeroConta, saldo)
+}
